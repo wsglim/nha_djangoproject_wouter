892c68981358f5526c2deeb0d87bcda2d553ea88
wouterglimmerveen <wouterglimmerveen@live.nl>
2026-01-27 10:54:21 +0100

    website toevoegingen

--- mijnvenv/mysite/blog/models.py
+++ mijnvenv/mysite/blog/models.py
@@ -17,16 +17,4 @@ class Post(models.Model):
         return self.title
 
 
-class Voetbalspelers(models.Model):   
-    naam = models.CharField(max_length=200)
-    actuele_voetbalclub = models.CharField(max_length=200)
-    auteur = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
 
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.naam
